4b8db4eb67b9bf5d7813f6744c58dae9e67f24f7
theroggy <pieter.roggemans@gmail.com>
2019-03-18 12:48:10 +0100

    Create run_sealedsurfaces_predict.py

---
+++ run_sealedsurfaces_predict.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Module to run a prediction.
+
+@author: Pieter Roggemans
+"""
+
+import predict_helper as ph
+
+#-------------------------------------------------------------
+# The real work
+#-------------------------------------------------------------
+
+def main():
+
+    ph.run_prediction(segment_config_filepaths=['general.ini', 
+                                                'sealedsurfaces.ini',
+                                                'local_overrule.ini'], 
+                      force_model_traindata_version=None)
+    
+if __name__ == '__main__':
+    main()
