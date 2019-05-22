0921cc2c3e14f49cc5062f7b66621605589ad429
theroggy <pieter.roggemans@gmail.com>
2019-05-22 10:31:46 +0200

    New segment_subject: solar panels

---
+++ bin/solarpanels_train.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Script with commands to run a training or prediction session.
+
+@author: Pieter Roggemans
+"""
+
+import os
+
+# Because orthoseg isn't installed as package + it is higher in dir hierarchy, add root to sys.path
+import sys
+sys.path.insert(0, '.')
+
+import orthoseg.train as train
+import orthoseg.predict as pred
+
+#-------------------------------------------------------------
+# The real work
+#-------------------------------------------------------------
+
+def main():
+        
+    # Start the training session
+    scriptdir = os.path.dirname(os.path.abspath(__file__))
+    train.run_training_session(segment_config_filepaths=[os.path.join(scriptdir, 'general.ini'), 
+                                                         os.path.join(scriptdir, 'solarpanels.ini'),
+                                                         os.path.join(scriptdir, 'local_overrule.ini')],
+                               force_traindata_version=None,
+                               resume_train=False)
+    pred.run_prediction(segment_config_filepaths=[os.path.join(scriptdir, 'general.ini'), 
+                                                  os.path.join(scriptdir, 'solarpanels.ini'),
+                                                  os.path.join(scriptdir, 'local_overrule.ini')], 
+                        force_model_traindata_version=None)
+
+if __name__ == '__main__':
+    main()
