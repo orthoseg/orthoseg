3480a1c0bcd54972b4fd9f376c39f76e1126f4cd
theroggy <pieter.roggemans@gmail.com>
2019-08-30 10:28:28 +0200

    Move (most) postprocessing of predictions to a seperate process

--- orthoseg/predict.py
+++ orthoseg/predict.py
@@ -16,18 +16,18 @@ import orthoseg.model.model_helper as mh
 import orthoseg.segment as segment
 import orthoseg.predict_postprocess as postp
 
-def run_prediction(segment_config_filepaths: str):
+def run_prediction(config_filepaths: str):
     """
     Run a prediction of the input dir given.
     
     Args
-        segment_config_filepaths: config files to use for the segmentation
+        config_filepaths: config files to use for the segmentation
     """
     
     # TODO: add something to delete old data, predictions???
 
     # Read the configuration
-    conf.read_config(segment_config_filepaths)
+    conf.read_config(config_filepaths)
     
     # Main initialisation of the logging
     logger = log_helper.main_log_init(conf.dirs['log_dir'], __name__)      
@@ -83,13 +83,3 @@ def run_prediction(segment_config_filepaths: str):
                         input_mask_dir=None,
                         batch_size=int(conf.predict['batch_size']),
                         evaluate_mode=False)
-    
-    # Now postprocess the vector results, so the end result is one big file
-    output_vector_base_dir = conf.dirs['output_vector_dir']
-    output_base_name = f"{conf.general['segment_subject']}_{conf.predict['image_datasource_code']}_{model_traindata_version:02d}"
-    output_vector_dir = os.path.join(output_vector_base_dir, output_base_name)
-    output_filepath = os.path.join(output_vector_dir, f"{output_base_name}.gpkg")
-    postp.postprocess_vectors(input_dir=predict_output_dir,
-                              output_filepath = output_filepath,
-                              evaluate_mode=False,
-                              force=False)
