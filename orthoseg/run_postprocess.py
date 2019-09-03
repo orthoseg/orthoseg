37982d8023060fca39d515a919d505acf5ea9888
theroggy <pieter.roggemans@gmail.com>
2019-09-03 15:18:43 +0200

    Fix the search for the postprocess input dir

--- orthoseg/run_postprocess.py
+++ orthoseg/run_postprocess.py
@@ -3,6 +3,7 @@
 Module with functions for post-processing prediction masks towards polygons.
 """
 
+import glob
 import os
 # TODO: the init of this doensn't seem to work properly... should be solved somewhere else?
 os.environ["GDAL_DATA"] = r"C:\Tools\anaconda3\envs\orthoseg4\Library\share\gdal"
@@ -16,7 +17,7 @@ from orthoseg.helpers import config_helper as conf
 from orthoseg.helpers import log_helper
 
 def postprocess_predictions(
-            config_filepaths: [str]):
+        config_filepaths: [str]):
     
     ##### Init #####
     # Main initialisation of the logging
@@ -24,15 +25,20 @@ def postprocess_predictions(
     logger = log_helper.main_log_init(conf.dirs['log_dir'], __name__)      
     logger.info(f"Config used: \n{conf.pformat_config()}")
 
-    # Input and output dir
-    input_dir = (conf.dirs['predict_image_output_basedir'] 
-                 + "_greenhouses_36_inceptionresnetv2+unet_373")
-    output_vector_name = f"{conf.general['segment_subject']}_{conf.predict['image_datasource_code']}_36_v2"
+    # Input dir = the "most recent" prediction result dir for this subject 
+    prediction_basedir = f"{conf.dirs['predict_image_output_basedir']}_{conf.general['segment_subject']}_"
+    prediction_dirs = sorted(glob.glob(f"{prediction_basedir}*{os.sep}"), reverse=True)
+    input_dir = prediction_dirs[0]
+	
+    # Format output dit, partly based on input dir
+    train_version = input_dir.replace(prediction_basedir, "").split('_')[0]
+    output_vector_name = f"{conf.general['segment_subject']}_{conf.predict['image_datasource_code']}_{train_version}"
     output_dir = os.path.join(
             conf.dirs['output_vector_dir'], output_vector_name)
     output_filepath = os.path.join(
             output_dir, f"{output_vector_name}.gpkg")
     
+    ##### Go! #####
     border_pixels_to_ignore = conf.predict.getint('image_pixels_overlap')
     postp.postprocess_predictions(
             input_dir=input_dir,
