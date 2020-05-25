a77d2e681e914ad0a3d29e0154e835ccb2ff7757
theroggy <pieter.roggemans@gmail.com>
2020-05-25 22:01:52 +0200

    Improvements to packaging to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="orthoseg", 
-    version="0.0.5",
+    version="0.0.9",
     author="Pieter Roggemans",
     author_email="pieter.roggemans@gmail.com",
     description="Package to make it easier to segment orthophotos.",
@@ -13,13 +13,17 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/theroggy/orthoseg",
     packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+            "tensorflow-gpu", "pillow", "rasterio", "geopandas", 
+            "owslib", "segmentation-models", "geofile-ops"],
     entry_points='''
-        [console_scripts]
-        orthoseg=orthoseg.orthoseg:main
-        ''',
+            [console_scripts]
+            orthoseg=orthoseg.orthoseg:main
+            ''',
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
 )
