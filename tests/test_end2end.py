d41295e133d4ce9303c594d74e2da2602dcadd6c
Pieter Roggemans <pieter.roggemans@gmail.com>
2022-12-03 22:35:26 +0100

    Cleanup

--- tests/test_end2end.py
+++ tests/test_end2end.py
@@ -17,11 +17,14 @@ import pytest
 root_dir = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(root_dir))
 import orthoseg
-from orthoseg import load_sampleprojects  
 
 from orthoseg.helpers import config_helper as conf
 import orthoseg.model.model_helper as mh
 from orthoseg.util import gdrive_util
+
+# ----------------------------------------------------
+# Init
+# ----------------------------------------------------
 
 sampleprojects_dir = root_dir / "sample_projects"
 testprojects_dir = Path(tempfile.gettempdir()) / "orthoseg/sample_projects"
@@ -32,11 +35,13 @@ projecttemplate_dir = testprojects_dir / "project_template"
 # Tests
 # ----------------------------------------------------
 
+
 def get_testdata_dir() -> Path:
     return Path(__file__).resolve().parent / "data"
 
-    
+
 def test_1_init_testproject():
+    # Use footballfields sample project for these end to end tests
     shutil.rmtree(testprojects_dir, ignore_errors=True)
     shutil.copytree(sampleprojects_dir, testprojects_dir)
 
@@ -52,13 +57,13 @@ def test_2_load_images():
     if image_cache_dir.exists():
         shutil.rmtree(image_cache_dir)
         # Make sure is is deleted now!
-        assert image_cache_dir.exists() is False
+        assert not image_cache_dir.exists()
 
     # Run task to load images
     orthoseg.load_images(config_path)
 
     # Check if the right number of files was loaded
-    assert image_cache_dir.exists() is True
+    assert image_cache_dir.exists()
     files = list(image_cache_dir.glob("**/*.jpg"))
     assert len(files) == 8
 
@@ -87,17 +92,15 @@ def test_3_train():
 
     # Make sure the label files in version 01 are older than those in the label dir
     # so a new model will be trained
-    """
     label_01_path = training_dir / "01/footballfields_BEFL-2019_polygons.gpkg"
     timestamp_old = datetime(year=2020, month=1, day=1).timestamp()
     os.utime(label_01_path, (timestamp_old, timestamp_old))
-    """
 
     # Run train session
     orthoseg.train(config_path)
 
     # Check if the training (image) data was created
-    assert training_id_dir.exists() is True
+    assert training_id_dir.exists()
 
     # Check if the new model was created
     best_model = mh.get_best_model(
@@ -130,26 +133,26 @@ def test_4_predict():
     if predict_image_output_dir.exists():
         shutil.rmtree(predict_image_output_dir)
         # Make sure it is deleted now!
-        assert predict_image_output_dir.exists() is False
+        assert not predict_image_output_dir.exists()
     result_vector_dir = conf.dirs.getpath("output_vector_dir")
     if result_vector_dir.exists():
         shutil.rmtree(result_vector_dir)
         # Make sure is is deleted now!
-        assert result_vector_dir.exists() is False
+        assert not result_vector_dir.exists()
 
     # Download the version 01 model
     model_dir = conf.dirs.getpath("model_dir")
     model_dir.mkdir(parents=True, exist_ok=True)
     model_hdf5_path = model_dir / "footballfields_01_0.97392_201.hdf5"
-    if model_hdf5_path.exists() is False:
+    if not model_hdf5_path.exists():
         gdrive_util.download_file("1UlNorZ74ADCr3pL4MCJ_tnKRNoeZX79g", model_hdf5_path)
     model_hyperparams_path = model_dir / "footballfields_01_hyperparams.json"
-    if model_hyperparams_path.exists() is False:
+    if not model_hyperparams_path.exists():
         gdrive_util.download_file(
             "1NwrVVjx9IsjvaioQ4-bkPMrq7S6HeWIo", model_hyperparams_path
         )
     model_modeljson_path = model_dir / "footballfields_01_model.json"
-    if model_modeljson_path.exists() is False:
+    if not model_modeljson_path.exists():
         gdrive_util.download_file(
             "1LNPLypM5in3aZngBKK_U4Si47Oe97ZWN", model_modeljson_path
         )
@@ -159,7 +162,7 @@ def test_4_predict():
 
     # Check results
     result_vector_path = result_vector_dir / "footballfields_01_201_BEFL-2019.gpkg"
-    assert result_vector_path.exists() is True
+    assert result_vector_path.exists()
     result_gdf = gfo.read_file(result_vector_path)
     assert len(result_gdf) == 356
 
@@ -186,6 +189,6 @@ def test_5_postprocess():
     orthoseg.postprocess(config_path)
 
     # Check results
-    assert result_diss_path.exists() is True
+    assert result_diss_path.exists()
     result_gdf = gfo.read_file(result_diss_path)
     assert len(result_gdf) == 350
