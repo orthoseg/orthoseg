af242c87d6e5cce75ac5150438d149a3052951e8
theroggy <pieter.roggemans@gmail.com>
2023-04-13 09:54:17 +0200

    Move + use ProgressLogger

--- orthoseg/util/ows_util.py
+++ orthoseg/util/ows_util.py
@@ -5,7 +5,6 @@ easier.
 """
 
 import concurrent.futures as futures
-import datetime
 import logging
 import math
 from pathlib import Path
@@ -29,6 +28,8 @@ import rasterio.errors as rio_errors
 from rasterio import profiles as rio_profiles
 from rasterio import transform as rio_transform
 from rasterio import windows as rio_windows
+
+from . import progress_util
 
 # -------------------------------------------------------------
 # First define/init some general variables/constants
@@ -270,11 +271,10 @@ def get_images_for_grid(
                     f"load_images to {output_image_dir.parent.name}/"
                     f"{output_image_dir.name}"
                 )
-                progress = ProgressHelper(
+                progress = progress_util.ProgressLogger(
                     message=message,
                     nb_steps_total=nb_total,
                     nb_steps_done=0,
-                    steps_before_first_reporting=10,
                 )
 
             nb_processed += 1
@@ -378,88 +378,6 @@ def get_images_for_grid(
 
                 # Sleep a bit before checking again if there are downloads ready
                 time.sleep(0.1)
-
-
-class ProgressHelper:
-    first_reporting_done = False
-
-    def __init__(
-        self,
-        message: str,
-        nb_steps_total: int,
-        nb_steps_done: int = 0,
-        start_time: Optional[datetime.datetime] = None,
-        time_between_reporting_s: int = 60,
-        steps_before_first_reporting: int = 1,
-        calculate_eta_since_lastreporting: bool = True,
-    ):
-        self.message = message
-        if start_time is None:
-            self.start_time = datetime.datetime.now()
-        else:
-            self.start_time = start_time
-        self.start_time_lastreporting = self.start_time
-        self.nb_steps_total = nb_steps_total
-        self.nb_steps_done = nb_steps_done
-        self.nb_steps_done_lastreporting = nb_steps_done
-        self.time_between_reporting_s = time_between_reporting_s
-        self.steps_before_first_reporting = steps_before_first_reporting
-        self.calculate_eta_since_lastreporting = calculate_eta_since_lastreporting
-
-    def step(self, message: Optional[str] = None, nb_steps: int = 1):
-
-        # Increase done counter
-        self.nb_steps_done += nb_steps
-
-        # Calculate time since last reporting
-        time_now = datetime.datetime.now()
-        time_passed_lastprogress_s = (
-            time_now - self.start_time_lastreporting
-        ).total_seconds()
-
-        # Calculate the time_passed and nb_done we want to use to calculate ETA
-        if self.calculate_eta_since_lastreporting is True:
-            time_passed_for_eta_s = (
-                time_now - self.start_time_lastreporting
-            ).total_seconds()
-            nb_steps_done_eta = self.nb_steps_done - self.nb_steps_done_lastreporting
-        else:
-            time_passed_for_eta_s = (time_now - self.start_time).total_seconds()
-            nb_steps_done_eta = self.nb_steps_done
-
-        # Print progress if time between reporting has passed or if minimum steps for
-        # first reporting have passed
-        if time_passed_lastprogress_s >= self.time_between_reporting_s or (
-            self.first_reporting_done is False
-            and self.nb_steps_done >= self.steps_before_first_reporting
-        ):
-            # Evade divisions by zero
-            if time_passed_for_eta_s == 0 or nb_steps_done_eta == 0:
-                return
-
-            nb_per_hour = (nb_steps_done_eta / time_passed_for_eta_s) * 3600
-            hours_to_go = (int)(
-                (self.nb_steps_total - self.nb_steps_done) / nb_per_hour
-            )
-            min_to_go = (int)(
-                (((self.nb_steps_total - self.nb_steps_done) / nb_per_hour) % 1) * 60
-            )
-            if message is not None:
-                progress_message = (
-                    f"{message}, {self.nb_steps_done}/{self.nb_steps_total}, "
-                    f"{hours_to_go:3d}:{min_to_go:2d} left at {nb_per_hour:0.0f}/h"
-                )
-            else:
-                progress_message = (
-                    f"{self.message}, {self.nb_steps_done}/{self.nb_steps_total}, "
-                    f"{hours_to_go:3d}:{min_to_go:2d} left at {nb_per_hour:0.0f}/h"
-                )
-            logger.info(progress_message)
-
-            self.start_time_lastreporting = time_now
-            self.nb_steps_done_lastreporting = self.nb_steps_done
-            if self.first_reporting_done is False:
-                self.first_reporting_done = True
 
 
 def _interprete_ssl_verify(ssl_verify: Union[bool, str, None]):
