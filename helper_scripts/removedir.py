04af5f64b108c8dde87ac7404759f6b3785d68d2
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-07-20 17:40:48 +0200

    Several small improvements to logging, documentation,... (#122)

--- helper_scripts/removedir.py
+++ helper_scripts/removedir.py
@@ -1,21 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Some tests for different things...
+Delete a directory faster (in windows)
 """
 
 import shutil
 import datetime as dt
 
-# If the script is ran directly...
-if __name__ == "__main__":
+del_dir = "X:/Monitoring/OrthoSeg/trees"
 
-    # Voorspelling in windows:
-    #    * 1 uur voorspellen,
-    #    * 3 uur deleten...
-    del_dir = "X:\\Monitoring\\OrthoSeg\\trees"
-
-    time_start = dt.datetime.now()
-    print(f"Start delete at {time_start}")
-    shutil.rmtree(del_dir)
-    time_taken = (dt.datetime.now() - time_start).total_seconds() / 3600
-    print(f"Time taken: {time_taken:.5f hours}")
+time_start = dt.datetime.now()
+print(f"Start delete at {time_start}")
+shutil.rmtree(del_dir)
+time_taken = (dt.datetime.now() - time_start).total_seconds() / 3600
+print(f"Time taken: {time_taken:.5f hours}")
