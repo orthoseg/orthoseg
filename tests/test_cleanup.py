1d11b508fc6c1bc2fe83bf4a6508638394139685
Kris Van Wayenberge <Kris.VanWayenberge@lv.vlaanderen.be>
2024-03-25 12:49:08 +0100

    - refactor test_cleanup

--- tests/test_cleanup.py
+++ tests/test_cleanup.py
@@ -43,27 +43,25 @@ def test_1_init_testproject():
 
 
 @pytest.mark.parametrize(
-    "type, simulate, versions_to_retain",
+    "type, simulate, versions_to_retain, expected_files_in_dir",
     [
-        ("model", True, 3),
-        ("training", True, 2),
-        ("prediction", True, 1),
+        ("model", True, 3, 25),
+        ("training", True, 2, 5),
+        ("prediction", True, 1, 10),
+        ("model", False, 3, 15),
+        ("training", False, 2, 2),
+        ("prediction", False, 1, 2),
     ],
 )
 @pytest.mark.order(after="test_1_init_testproject")
-def test_2_cleanup_simulate(type: str, simulate: bool, versions_to_retain: int):
+def test_cleanup(
+    type: str, simulate: bool, versions_to_retain: int, expected_files_in_dir: int
+):
     config_path = cleanup_dir / "cleanup_BEFL-2019.ini"
     config_overrules = [
         f"cleanup.simulate={simulate}",
         f"cleanup.{type}_versions_to_retain={versions_to_retain}",
     ]
-    models_dir = cleanup_dir / "models"
-    models_orig = os.listdir(models_dir)
-    predictions_dir = cleanup_dir / "output_vector"
-    prediction_dir = os.listdir(predictions_dir)[0]
-    predictions_orig = os.listdir(predictions_dir / prediction_dir)
-    training_dir = cleanup_dir / "training"
-    training_orig = os.listdir(training_dir)
 
     match type:
         case "model":
@@ -71,72 +69,21 @@ def test_2_cleanup_simulate(type: str, simulate: bool, versions_to_retain: int):
                 config_path=config_path / config_path,
                 config_overrules=config_overrules,
             )
-            models = os.listdir(models_dir)
-            assert len(models) == len(models_orig)
+            models = os.listdir(cleanup_dir / "models")
+            assert len(models) == expected_files_in_dir
         case "training":
             cleanup.clean_training_data_directories(
                 config_path=config_path / config_path,
                 config_overrules=config_overrules,
             )
-            training_dirs = os.listdir(training_dir)
-            assert len(training_dirs) == len(training_orig)
+            training_dirs = os.listdir(cleanup_dir / "training")
+            assert len(training_dirs) == expected_files_in_dir
         case "prediction":
             cleanup.clean_predictions(
                 config_path=config_path / config_path,
                 config_overrules=config_overrules,
             )
-            prediction_dirs = os.listdir(predictions_dir)
+            prediction_dirs = os.listdir(cleanup_dir / "output_vector")
             for prediction_dir in prediction_dirs:
-                predictions = os.listdir(predictions_dir / prediction_dir)
-                assert len(predictions) == len(predictions_orig)
-
-
-@pytest.mark.parametrize(
-    "type, simulate, versions_to_retain",
-    [
-        ("model", False, 3),
-        ("training", False, 2),
-        ("prediction", False, 1),
-    ],
-)
-@pytest.mark.order(after="test_2_cleanup_simulate")
-def test_3_cleanup(type: str, simulate: bool, versions_to_retain: int):
-    config_path = cleanup_dir / "cleanup_BEFL-2019.ini"
-    config_overrules = [
-        f"cleanup.simulate={simulate}",
-        f"cleanup.{type}_versions_to_retain={versions_to_retain}",
-    ]
-    models_dir = cleanup_dir / "models"
-    models_orig = os.listdir(models_dir)
-    predictions_dir = cleanup_dir / "output_vector"
-    prediction_dir = os.listdir(predictions_dir)[0]
-    predictions_orig = os.listdir(predictions_dir / prediction_dir)
-    training_dir = cleanup_dir / "training"
-    training_orig = os.listdir(training_dir)
-
-    match type:
-        case "model":
-            cleanup.clean_models(
-                config_path=config_path / config_path,
-                config_overrules=config_overrules,
-            )
-            models = os.listdir(models_dir)
-            assert len(models) == versions_to_retain * len(models_orig) / 5
-        case "training":
-            cleanup.clean_training_data_directories(
-                config_path=config_path / config_path,
-                config_overrules=config_overrules,
-            )
-            training_dirs = os.listdir(training_dir)
-            assert len(training_dirs) == versions_to_retain * len(training_orig) / 5
-        case "prediction":
-            cleanup.clean_predictions(
-                config_path=config_path / config_path,
-                config_overrules=config_overrules,
-            )
-            prediction_dirs = os.listdir(predictions_dir)
-            for prediction_dir in prediction_dirs:
-                predictions = os.listdir(predictions_dir / prediction_dir)
-                assert (
-                    len(predictions) == versions_to_retain * len(predictions_orig) / 5
-                )
+                predictions = os.listdir(cleanup_dir / "output_vector" / prediction_dir)
+                assert len(predictions) == expected_files_in_dir
