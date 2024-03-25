34487ed4e348f7ad3ca72853e273085e90dbab31
Kris Van Wayenberge <Kris.VanWayenberge@lv.vlaanderen.be>
2024-03-25 08:45:28 +0100

    - add test cleanup simulate

--- tests/test_cleanup.py
+++ tests/test_cleanup.py
@@ -45,13 +45,62 @@ def test_1_init_testproject():
 @pytest.mark.parametrize(
     "type, simulate, versions_to_retain",
     [
-        ("model", False, 1),
-        ("training", False, 1),
-        ("prediction", False, 1),
+        ("model", True, 3),
+        ("training", True, 2),
+        ("prediction", True, 1),
     ],
 )
 @pytest.mark.order(after="test_1_init_testproject")
-def test_2_cleanup(type: str, simulate: bool, versions_to_retain: int):
+def test_2_cleanup_simulate(type: str, simulate: bool, versions_to_retain: int):
+    config_path = cleanup_dir / "cleanup_BEFL-2019.ini"
+    config_overrules = [
+        f"cleanup.simulate={simulate}",
+        f"cleanup.{type}_versions_to_retain={versions_to_retain}",
+    ]
+    models_dir = cleanup_dir / "models"
+    models_orig = os.listdir(models_dir)
+    predictions_dir = cleanup_dir / "output_vector"
+    prediction_dir = os.listdir(predictions_dir)[0]
+    predictions_orig = os.listdir(predictions_dir / prediction_dir)
+    training_dir = cleanup_dir / "training"
+    training_orig = os.listdir(training_dir)
+
+    match type:
+        case "model":
+            cleanup.clean_models(
+                config_path=config_path / config_path,
+                config_overrules=config_overrules,
+            )
+            models = os.listdir(models_dir)
+            assert len(models) == len(models_orig)
+        case "training":
+            cleanup.clean_training_data_directories(
+                config_path=config_path / config_path,
+                config_overrules=config_overrules,
+            )
+            training_dirs = os.listdir(training_dir)
+            assert len(training_dirs) == len(training_orig)
+        case "prediction":
+            cleanup.clean_predictions(
+                config_path=config_path / config_path,
+                config_overrules=config_overrules,
+            )
+            prediction_dirs = os.listdir(predictions_dir)
+            for prediction_dir in prediction_dirs:
+                predictions = os.listdir(predictions_dir / prediction_dir)
+                assert len(predictions) == len(predictions_orig)
+
+
+@pytest.mark.parametrize(
+    "type, simulate, versions_to_retain",
+    [
+        ("model", False, 3),
+        ("training", False, 2),
+        ("prediction", False, 1),
+    ],
+)
+@pytest.mark.order(after="test_2_cleanup_simulate")
+def test_3_cleanup(type: str, simulate: bool, versions_to_retain: int):
     config_path = cleanup_dir / "cleanup_BEFL-2019.ini"
     config_overrules = [
         f"cleanup.simulate={simulate}",
