d2f36578ad08bfa713daff6d18c7a540249bbe5e
Kris Van Wayenberge <Kris.VanWayenberge@lv.vlaanderen.be>
2024-04-17 11:24:08 +0200

    - use tmp_path to create project files

--- tests/test_cleanup.py
+++ tests/test_cleanup.py
@@ -2,7 +2,6 @@ import logging
 import os
 from pathlib import Path
 import shutil
-import tempfile
 
 import pytest
 
@@ -10,27 +9,27 @@ from orthoseg.lib import cleanup
 from orthoseg.helpers import config_helper as conf
 from tests import test_helper
 
-testprojects_dir = Path(tempfile.gettempdir()) / "orthoseg_test_cleanup"
-sampleprojects_dir = testprojects_dir / "sample_projects"
-footballfields_dir = sampleprojects_dir / "footballfields"
 
+def create_projects_dir(tmp_path: Path) -> Path:
+    testproject_dir = tmp_path / "orthoseg_test_cleanup"
+    project_dir = testproject_dir / "footballfields"
 
-def init_project():
-    shutil.rmtree(path=testprojects_dir, ignore_errors=True)
-    footballfields_dir.mkdir(parents=True, exist_ok=True)
+    shutil.rmtree(path=testproject_dir, ignore_errors=True)
+    project_dir.mkdir(parents=True, exist_ok=True)
     shutil.copyfile(
         src=test_helper.sampleprojects_dir / "project_template/projectfile.ini",
-        dst=footballfields_dir / "footballfields.ini",
+        dst=project_dir / "footballfields.ini",
     )
     shutil.copyfile(
         src=test_helper.sampleprojects_dir / "imagelayers.ini",
-        dst=sampleprojects_dir / "imagelayers.ini",
+        dst=testproject_dir / "imagelayers.ini",
     )
+    return project_dir
 
 
-def load_project_config():
+def load_project_config(path: Path):
     conf.read_orthoseg_config(
-        config_path=footballfields_dir / "footballfields.ini",
+        config_path=path / "footballfields.ini",
         overrules=[
             "general.segment_subject=footballfields",
             "predict.image_layer=BEFL_2019",
@@ -46,11 +45,13 @@ def load_project_config():
     ],
 )
 def test_cleanup_models(
-    simulate: bool, versions_to_retain: int, expected_files_in_dir: int
+    tmp_path, simulate: bool, versions_to_retain: int, expected_files_in_dir: int
 ):
-    # Init test project
-    init_project()
-    models_dir = footballfields_dir / "models"
+    # Create test project
+    project_dir = create_projects_dir(tmp_path=tmp_path)
+
+    # Creating dummy files
+    models_dir = project_dir / "models"
     models_dir.mkdir(parents=True, exist_ok=True)
     models = [
         "0.44293_0.hdf5",
@@ -61,17 +62,18 @@ def test_cleanup_models(
     ]
     for x in range(1, 6):
         for model in models:
-            (models_dir / f"footballfields_0{x}_{model}").touch
+            (models_dir / f"footballfields_0{x}_{model}").touch()
 
     # Load project config to init some vars.
-    load_project_config()
+    load_project_config(path=project_dir)
 
+    # Cleanup
     cleanup.clean_models(
         model_dir=conf.dirs.getpath("model_dir"),
         versions_to_retain=versions_to_retain,
         simulate=simulate,
     )
-    models = os.listdir(footballfields_dir / "models")
+    models = os.listdir(project_dir / "models")
     assert len(models) == expected_files_in_dir
 
 
@@ -83,27 +85,30 @@ def test_cleanup_models(
     ],
 )
 def test_cleanup_training(
-    simulate: bool, versions_to_retain: int, expected_files_in_dir: int
+    tmp_path, simulate: bool, versions_to_retain: int, expected_files_in_dir: int
 ):
-    # Init test project
-    init_project()
-    training_dir = footballfields_dir / "training"
+    # Create test project
+    project_dir = create_projects_dir(tmp_path=tmp_path)
+
+    # Creating dummy files
+    training_dir = project_dir / "training"
     training_dir.mkdir(parents=True, exist_ok=True)
     for x in range(1, 6):
         dir = training_dir / f"0{x}"
         dir.mkdir(parents=True, exist_ok=True)
-        (training_dir / dir / "footballfields_BEFL-2019_locations.gpkg").touch
-        (training_dir / dir / "footballfields_BEFL-2019_polygons.gpkg").touch
+        (training_dir / dir / "footballfields_BEFL-2019_locations.gpkg").touch()
+        (training_dir / dir / "footballfields_BEFL-2019_polygons.gpkg").touch()
 
     # Load project config to init some vars.
-    load_project_config()
+    load_project_config(path=project_dir)
 
+    # Cleanup
     cleanup.clean_training_data_directories(
         training_dir=conf.dirs.getpath("training_dir"),
         versions_to_retain=versions_to_retain,
         simulate=simulate,
     )
-    training_dirs = os.listdir(footballfields_dir / "training")
+    training_dirs = os.listdir(project_dir / "training")
     assert len(training_dirs) == expected_files_in_dir
 
 
@@ -115,11 +120,13 @@ def test_cleanup_training(
     ],
 )
 def test_cleanup_predictions(
-    simulate: bool, versions_to_retain: int, expected_files_in_dir: int
+    tmp_path, simulate: bool, versions_to_retain: int, expected_files_in_dir: int
 ):
-    # Init test project
-    init_project()
-    predictions_dir = footballfields_dir / "output_vector"
+    # Create test project
+    project_dir = create_projects_dir(tmp_path=tmp_path)
+
+    # Creating dummy files
+    predictions_dir = project_dir / "output_vector"
     predictions_dir.mkdir(parents=True, exist_ok=True)
     for x in range(1, 6):
         dir = predictions_dir / f"BEFL_20{18 + x}"
@@ -127,24 +134,25 @@ def test_cleanup_predictions(
         for p in range(1, 6):
             (
                 predictions_dir / dir / f"footballfields_0{p}_201_BEFL-20{18 + x}.gpkg"
-            ).touch
+            ).touch()
             (
                 predictions_dir
                 / dir
                 / f"footballfields_0{p}_201_BEFL-20{18 + x}_dissolve.gpkg"
-            ).touch
+            ).touch()
 
     # Load project config to init some vars.
-    load_project_config()
+    load_project_config(path=project_dir)
 
+    # Cleanup
     cleanup.clean_predictions(
         output_vector_dir=conf.dirs.getpath("output_vector_dir"),
         versions_to_retain=versions_to_retain,
         simulate=simulate,
     )
-    prediction_dirs = os.listdir(footballfields_dir / "output_vector")
+    prediction_dirs = os.listdir(project_dir / "output_vector")
     for prediction_dir in prediction_dirs:
-        predictions = os.listdir(footballfields_dir / "output_vector" / prediction_dir)
+        predictions = os.listdir(project_dir / "output_vector" / prediction_dir)
         assert len(predictions) == expected_files_in_dir
 
 
