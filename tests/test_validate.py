a4fa6d172f7059a9f56fb3e786b601ddc674ac39
Kris Van Wayenberge <Kris.VanWayenberge@lv.vlaanderen.be>
2024-04-26 11:33:53 +0200

    - refactor

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 import shutil
 import pytest
 
@@ -28,43 +27,28 @@ def test_validate_args(args):
 
 def test_validate_error(tmp_path):
     # Create test project
-    project_dir = create_projects_dir(tmp_path=tmp_path)
+    project_dir = tmp_path / "footballfields"
+    project_dir.mkdir(parents=True, exist_ok=True)
+    shutil.copyfile(
+        src=test_helper.SampleProjectFootball.predict_config_path,
+        dst=project_dir / "footballfields_BEFL-2019_test.ini",
+    )
+    shutil.copyfile(
+        src=test_helper.SampleProjectFootball.project_dir
+        / "footballfields_BEFL-2019.ini",
+        dst=project_dir / "footballfields_BEFL-2019.ini",
+    )
     # Create dummy files
     training_dir = project_dir / "training"
-    create_training_files(path=training_dir)
-    overrules = [
-        "general.segment_subject=footballfields",
-        "predict.image_layer=BEFL-2019",
-    ]
+    sub_dir = training_dir / "01"
+    sub_dir.mkdir(parents=True, exist_ok=True)
+    (sub_dir / "footballfields_BEFL-2019_locations.gpkg").touch()
+    (sub_dir / "footballfields_BEFL-2019_polygons.gpkg").touch()
 
     with pytest.raises(
         Exception,
         match="ERROR while running validate for task footballfields",
     ):
         orthoseg.validate(
-            config_path=project_dir / "footballfields.ini", config_overrules=overrules
+            config_path=test_helper.SampleProjectFootball.predict_config_path,
         )
-
-
-def create_projects_dir(tmp_path: Path) -> Path:
-    testproject_dir = tmp_path / "orthoseg_test_validate"
-    project_dir = testproject_dir / "footballfields"
-
-    shutil.rmtree(path=testproject_dir, ignore_errors=True)
-    project_dir.mkdir(parents=True, exist_ok=True)
-    shutil.copyfile(
-        src=test_helper.sampleprojects_dir / "project_template/projectfile.ini",
-        dst=project_dir / "footballfields.ini",
-    )
-    shutil.copyfile(
-        src=test_helper.sampleprojects_dir / "imagelayers.ini",
-        dst=testproject_dir / "imagelayers.ini",
-    )
-    return project_dir
-
-
-def create_training_files(path: Path):
-    sub_dir = path / "01"
-    sub_dir.mkdir(parents=True, exist_ok=True)
-    (sub_dir / "footballfields_BEFL-2019_locations.gpkg").touch()
-    (sub_dir / "footballfields_BEFL-2019_polygons.gpkg").touch()
