273cd532d582bb437451c9115a361eac55af6c83
Kris Van Wayenberge <36810094+KriWay-LV@users.noreply.github.com>
2024-07-19 19:10:52 +0200

    Add option(s) to do automatic cleanup of old models, predictions and training datasets (#169)
    Co-authored-by: Pieter Roggemans <pieter.roggemans@gmail.com>

---
+++ helper_scripts/cleanup_old_data.py
@@ -0,0 +1,53 @@
+import logging
+import os
+from pathlib import Path
+
+from orthoseg.lib import cleanup
+
+from orthoseg.helpers import config_helper as conf
+from orthoseg.util import log_util
+
+logger = logging.getLogger(__name__)
+
+
+def cleanup_old_data(projects_dir: Path, simulate: bool = False):
+    # Exclude directories where name starts with '_'
+    projects = [
+        subdir for subdir in os.listdir(projects_dir)
+        if os.path.isdir(projects_dir/subdir) and not subdir.startswith("_")
+    ]
+    for project in projects:
+        config_path = projects_dir / project / f"{project}.ini"
+        if config_path.exists():
+            conf.read_orthoseg_config(config_path=config_path)
+            global logger
+            logger = log_util.main_log_init(
+                log_dir=conf.dirs.getpath("log_dir"),
+                log_basefilename=cleanup_old_data.__name__
+            )
+            cleanup.clean_project_dir(
+                model_dir=conf.dirs.getpath("model_dir"),
+                model_versions_to_retain=conf.cleanup.getint("model_versions_to_retain"),
+                training_dir=conf.dirs.getpath("training_dir"),
+                training_versions_to_retain=conf.cleanup.getint(
+                    "training_versions_to_retain"
+                ),
+                output_vector_dir=conf.dirs.getpath("output_vector_dir"),
+                prediction_versions_to_retain=conf.cleanup.getint(
+                    "prediction_versions_to_retain"
+                ),
+                simulate=simulate,
+            )
+        else:
+            # logger.info(f"Config_path ({config_path}) doesn't exist.")
+            print(f"Config_path ({config_path}) doesn't exist.")
+                
+        
+
+# If the script is ran directly...
+if __name__ == "__main__":
+    # cleanup()
+    cleanup_old_data(
+        projects_dir=Path(r"X:\Monitoring\OrthoSeg"),
+        simulate=True
+    )
---
+++ orthoseg/util/data.py
@@ -0,0 +1,113 @@
+"""
+Module with specific helper functions to get info for gisdata files.
+"""
+
+import logging
+from pathlib import Path
+
+
+logger = logging.getLogger(__name__)
+
+# ---------------------------------------------------------
+# Functions to get base paths of locations where GIS data
+# can be stored
+# ---------------------------------------------------------
+
+
+def aidetection_dir() -> Path:
+    """
+    AI detection path.
+
+    Returns:
+        Path: AI detection path
+    """
+    return Path("X:/Monitoring/orthoseg")
+
+
+# ---------------------------------------------------------
+# Functions to get info about AI detection files
+# ---------------------------------------------------------
+
+
+class AiDetectionInfo:
+    """
+    Information about an ai detection result.
+    """
+
+    def __init__(
+        self,
+        path: Path,
+        subject: str,
+        traindata_version: int,
+        image_layer: str,
+        image_layer_year: int,
+        postprocessing: str,
+    ):
+        """
+        Init.
+
+        Args:
+            path (Path): the path to the file
+            subject (str): the detection/segment subject
+            traindata_version (int): the version of the data used to train the model
+            image_layer (str): the image layer that the detection is based on
+            image_layer_year (int): the year the images in the image_layer were taken
+            postprocessing (str): the postprocessing done
+        """
+        self.path = path
+        self.subject = subject
+        self.traindata_version = traindata_version
+        self.image_layer = image_layer
+        self.image_layer_year = image_layer_year
+        self.postprocessing = postprocessing
+
+
+def aidetection_info(path: Path) -> AiDetectionInfo:
+    """
+    Get the properties from an ai detection filepath.
+
+    Args:
+        path (Path): the filepath to the ai detection file
+
+    Return:
+        AiDetectionInfo: info about the detection.
+    """
+    # Extract the fields...
+    try:
+        param_values = path.stem.split("_")
+        if len(param_values) < 4:
+            logger.warning(
+                "No valid path for an ai detection, split('_') should result in 4 "
+                f"fields: {path}"
+            )
+
+        subject = param_values[0]
+        model_info = param_values[1]
+        model_info_values = model_info.split(".")
+        traindata_version = int(model_info_values[0])
+        """
+        architecture_version = 0
+        trainparams_version = 0
+        if len(model_info_values) > 1:
+            architecture_version = int(model_info_values[1])
+            if len(model_info_values) > 2:
+                trainparams_version = int(model_info_values[2])
+        aimodel_epoch = int(param_values[2])
+        """
+        image_layer = param_values[3]
+        image_layer_values = image_layer.split("-")
+        try:
+            image_layer_year = int(image_layer_values[1])
+        except ValueError:
+            image_layer_year = None
+        if len(param_values) > 4:
+            postprocessing = "_".join(param_values[4:])
+        else:
+            postprocessing = ""
+
+    except Exception as ex:
+        raise ValueError(f"Error in get_aidetection_info on {path}") from ex
+
+    return AiDetectionInfo(
+        path, subject, traindata_version, image_layer, image_layer_year, postprocessing
+    )
