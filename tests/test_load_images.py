f16589f3fcd4e6d6f6fb3f27aee4c9a927606eac
KriWay <kris.vanwayenberge@bytz.be>
2024-10-07 13:23:32 +0200

    add support for xyz-service

--- tests/test_load_images.py
+++ tests/test_load_images.py
@@ -1,17 +1,26 @@
 import shutil
 
+import pytest
+
 import orthoseg
 from orthoseg.helpers import config_helper as conf
 from tests import test_helper
 
 
-def test_load_images_wmts(tmp_path):
+@pytest.mark.parametrize(
+    "football_ini",
+    [
+        "footballfields_BEFL-2023-WMTS_test.ini",
+        "footballfields_BEFL-2023-XYZ_test.ini",
+    ],
+)
+def test_load_images_wmts(tmp_path, football_ini: str):
     # Use footballfields sample project for these end to end tests
     testprojects_dir = tmp_path / "sample_projects"
     footballfields_dir = testprojects_dir / "footballfields"
     shutil.copytree(test_helper.sampleprojects_dir, testprojects_dir)
     # Load project config to init some vars.
-    config_path = footballfields_dir / "footballfields_BEFL-2023-WMTS_test.ini"
+    config_path = footballfields_dir / football_ini
     conf.read_orthoseg_config(config_path)
     image_cache_dir = conf.dirs.getpath("predict_image_input_dir")
 
