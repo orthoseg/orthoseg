675299ec3d2d7e83b8b96cd862efd47b45ef003d
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-12-03 16:31:05 +0100

    Fix end2end tests

--- tests/test_end2end.py
+++ tests/test_end2end.py
@@ -136,9 +136,9 @@ def test_4_predict():
     assert result_vector_path.exists()
     result_gdf = gfo.read_file(result_vector_path)
     if os.name == "nt":
-        assert len(result_gdf) == 211
+        assert len(result_gdf) == 184
     else:
-        assert len(result_gdf) == 211
+        assert len(result_gdf) == 184
 
 
 @pytest.mark.skipif(
@@ -166,6 +166,6 @@ def test_5_postprocess():
     assert result_diss_path.exists()
     result_gdf = gfo.read_file(result_diss_path)
     if os.name == "nt":
-        assert len(result_gdf) == 207
+        assert len(result_gdf) == 182
     else:
-        assert len(result_gdf) == 207
+        assert len(result_gdf) == 182
