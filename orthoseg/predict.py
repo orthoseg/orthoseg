332cafc3aa34b4d6745704e01d4ca8a746558d24
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-01-24 15:52:05 +0100

    ENH: include traindata id in status mails sent + clarify errors when thrown (#247)
    Added traindata_id in status mails sent for train, predict and
    postprocess.

--- orthoseg/predict.py
+++ orthoseg/predict.py
@@ -77,6 +77,7 @@ def predict(config_path: Path, config_overrules: list[str] = []):
     logger = log_util.main_log_init(conf.dirs.getpath("log_dir"), __name__)
     logger.info(f"Start predict for config {config_path.stem}")
     logger.debug(f"Config used: \n{conf.pformat_config()}")
+    model_name = None
 
     try:
         # Read some config, and check if values are ok
@@ -111,10 +112,12 @@ def predict(config_path: Path, config_overrules: list[str] = []):
                 f"traindata_id: {traindata_id}"
             )
             logger.critical(message)
-            raise Exception(message)
+            raise RuntimeError(message)
         else:
             model_weights_filepath = best_model["filepath"]
             logger.info(f"Best model found: {model_weights_filepath}")
+
+        model_name = f"{best_model['segment_subject']}_{best_model['traindata_id']}"
 
         # Load the hyperparams of the model
         # TODO: move the hyperparams filename formatting to get_models...
@@ -261,7 +264,7 @@ def predict(config_path: Path, config_overrules: list[str] = []):
         # Start predict for entire dataset
         # --------------------------------
         # Send email
-        email_helper.sendmail(f"Start predict for {config_path.stem} on {image_layer}")
+        email_helper.sendmail(f"Start predict for {model_name} on {image_layer}")
 
         # Check if we should use an image cache
         use_cache = image_layer_config.get("use_cache", "yes")
@@ -320,7 +323,7 @@ def predict(config_path: Path, config_overrules: list[str] = []):
             )
 
         # Log and send mail
-        message = f"Completed predict for {config_path.stem} on {image_layer}"
+        message = f"Completed predict for {model_name} on {image_layer}"
         logger.info(message)
         email_helper.sendmail(message)
 
@@ -341,12 +344,14 @@ def predict(config_path: Path, config_overrules: list[str] = []):
             simulate=conf.cleanup.getboolean("simulate"),
         )
     except Exception as ex:
-        message = f"ERROR in predict for {config_path.stem} on {image_layer}"
+        if model_name is None:
+            model_name = config_path.stem
+        message = f"ERROR in predict for {model_name} on {image_layer}"
         logger.exception(message)
         email_helper.sendmail(
             subject=message, body=f"Exception: {ex}\n\n {traceback.format_exc()}"
         )
-        raise RuntimeError(message) from ex
+        raise RuntimeError(f"{message}: {ex}") from ex
     finally:
         conf.remove_run_tmp_dir()
 
