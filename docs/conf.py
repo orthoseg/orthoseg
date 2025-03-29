359f0dd6ed33ac80e338825f1cb0889ed4023bb5
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-03-29 21:01:42 +0100

    Try to add to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,10 @@ author = "Pieter Roggemans"
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
+script_dir = Path(__name__).resolve()
+print(script_dir)
+lib_dir = "/home/docs/checkouts/readthedocs.org/user_builds/orthoseg/conda/259/lib"
+os.environ["PATH"] = f"{os.environ["PATH"]}:{lib_dir}"
 import orthoseg  # noqa: E402
 
 version = release = orthoseg.__version__
