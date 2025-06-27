5036a5ec6d784b77bed6e5455293f53a4a70cfa4
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-06-28 01:26:33 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -105,3 +105,6 @@ html_sidebars: dict[str, list[str]] = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# Get the base URL for the HTML documentation.
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
