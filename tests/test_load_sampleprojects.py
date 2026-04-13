6a4b11aaf28374ce0318e263201992419b5a381e
Pieter Roggemans <pieter.roggemans@gmail.com>
2026-04-13 10:23:39 +0200

    ENH-download-sample-projects-from-release

--- tests/test_load_sampleprojects.py
+++ tests/test_load_sampleprojects.py
@@ -35,15 +35,6 @@ def test_load_images_args(args, exp_ssl_verify):
         assert valid_args["ssl_verify"] == exp_ssl_verify
 
 
-@pytest.mark.skipif(
-    "GITHUB_ACTIONS" in os.environ
-    and not (
-        platform.system() == "Linux"
-        and sys.version_info.major == 3
-        and sys.version_info.minor == 10
-    ),
-    reason="on github CI, run this only in one env to avoid rate limit exceeded",
-)
 def test_load_sampleprojects(tmp_path):
     sampleprojects_dir = tmp_path / "sample_projects"
     shutil.rmtree(sampleprojects_dir, ignore_errors=True)
