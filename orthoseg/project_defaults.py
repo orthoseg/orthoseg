98ebbe683b4bc023aa24cdd795b028807a3ca963
Pieter Roggemans <pieter.roggemans@gmail.com>
2026-04-16 00:35:42 +0200

    Fix linter issues

--- orthoseg/project_defaults.py
+++ orthoseg/project_defaults.py
@@ -13,6 +13,7 @@ loaded config file.
 
 Generated from: /home/pierog/github/orthoseg/orthoseg/project_defaults.ini
 """
+
 from __future__ import annotations
 
 import dataclasses
@@ -32,7 +33,7 @@ class General:
     the project config file.
     """
 
-    segment_subject: str = 'MUST_OVERRIDE'
+    segment_subject: str = "MUST_OVERRIDE"
     """The subject that will be segmented.
 
     This value must be overruled in the project specific config file.
@@ -82,7 +83,7 @@ class Model:
     of trained models.
     """
 
-    architecture: str = 'inceptionresnetv2+unet'
+    architecture: str = "inceptionresnetv2+unet"
     """The segmentation architecture to use.
 
     The architectures currently supported by orthoseg follow the encoder-decoder
@@ -148,14 +149,18 @@ class Train:
 
     image_pixel_y_size: float = 0.25
 
-    labelpolygons_pattern: str = '${dirs:labels_dir}/${general:segment_subject}_{image_layer}_polygons.gpkg'  # noqa: E501
+    labelpolygons_pattern: str = (
+        "${dirs:labels_dir}/${general:segment_subject}_{image_layer}_polygons.gpkg"
+    )
     """Pattern how the file paths of the label files should be formatted.
     The image_layer to get the images from is extracted from the file path.
     """
 
-    labellocations_pattern: str = '${dirs:labels_dir}/${general:segment_subject}_{image_layer}_locations.gpkg'  # noqa: E501
+    labellocations_pattern: str = (
+        "${dirs:labels_dir}/${general:segment_subject}_{image_layer}_locations.gpkg"
+    )
 
-    labelname_column: str = 'classname'
+    labelname_column: str = "classname"
     """Column where the labels for each training polygon is available.
 
     If the column name configured here is not available columns "label_name"
@@ -208,10 +213,7 @@ class Train:
             "width_shift_range": 0.05,
             "height_shift_range": 0.05,
             "zoom_range": 0.1,
-            "brightness_range": [
-                0.95,
-                1.05
-            ]
+            "brightness_range": [0.95, 1.05],
         }
     )
     """The augmentations to apply to the input images during training.
@@ -235,10 +237,7 @@ class Train:
             "width_shift_range": 0.05,
             "height_shift_range": 0.05,
             "zoom_range": 0.1,
-            "brightness_range": [
-                1.0,
-                1.0
-            ]
+            "brightness_range": [1.0, 1.0],
         }
     )
     """The augmentations to apply to the label masks during training.
@@ -257,18 +256,13 @@ class Train:
     classes: dict = dataclasses.field(
         default_factory=lambda: {
             "background": {
-                "labelnames": [
-                    "ignore_for_train",
-                    "background"
-                ],
-                "weight": 1
+                "labelnames": ["ignore_for_train", "background"],
+                "weight": 1,
             },
             "${general:segment_subject}": {
-                "labelnames": [
-                    "${general:segment_subject}"
-                ],
-                "weight": 1
-            }
+                "labelnames": ["${general:segment_subject}"],
+                "weight": 1,
+            },
         }
     )
     """The classes to be trained to.
@@ -291,13 +285,11 @@ class Train:
     A proper values depends on available hardware, model used and image size.
     """
 
-    optimizer: str = 'adam'
+    optimizer: str = "adam"
     """Optimizer to use for training."""
 
     optimizer_params: dict = dataclasses.field(
-        default_factory=lambda: {
-            "learning_rate": 0.0001
-        }
+        default_factory=lambda: {"learning_rate": 0.0001}
     )
     """Parameters to use for the optimizer."""
 
@@ -312,7 +304,7 @@ class Train:
         - If no weights are specified: categorical_crossentropy
     """
 
-    monitor_metric: str = '({one_hot_mean_iou}+{val_one_hot_mean_iou})/2'
+    monitor_metric: str = "({one_hot_mean_iou}+{val_one_hot_mean_iou})/2"
     """The metric(s) to monitor to evaluate which network is best during training.
 
     This can be a single metric or a formula with placeholders
@@ -324,7 +316,7 @@ class Train:
     - val_categorical_accuracy: accuracy on the validation dataset
     """
 
-    monitor_metric_mode: str = 'max'
+    monitor_metric_mode: str = "max"
     """The mode of the monitor metric.
 
     Use max to keep the models with a high monitor_metric. Otherwise use min.
@@ -366,13 +358,13 @@ class Train:
     Not having improvement means the `earlystop_monitor_metric` hasn't improved.
     """
 
-    earlystop_monitor_metric: str = 'one_hot_mean_iou'
+    earlystop_monitor_metric: str = "one_hot_mean_iou"
     """Metric to use to determine that the model doesn't improve.
 
     Options are: categorical_accuracy, one_hot_mean_iou
     """
 
-    earlystop_monitor_metric_mode: str = 'max'
+    earlystop_monitor_metric_mode: str = "max"
     """The mode of the earlystop metric.
 
     Use max if the monitor_metric should be high. Otherwise use min.
@@ -402,7 +394,7 @@ class Predict:
     A proper values depends on available hardware, model used and image size.
     """
 
-    image_layer: str = 'MUST_OVERRIDE'
+    image_layer: str = "MUST_OVERRIDE"
     """The image layer to use for the prediction.
 
     The image layer needs to be an existing layer in imagelayers.ini.
@@ -462,7 +454,7 @@ class Predict:
         `reclassify_to_neighbour_query = (onborder == 0 and area <= 5)`
     """
 
-    simplify_algorithm: str = 'LANG+'
+    simplify_algorithm: str = "LANG+"
     """Algorithm to use for vector simplification.
 
     The simplification configured here will be executed on the fly during the
@@ -487,7 +479,7 @@ class Predict:
     * If simplify_algorithm is not specified, no simplification is applied.
     """
 
-    simplify_tolerance: str = '${image_pixel_x_size}*1.5'
+    simplify_tolerance: str = "${image_pixel_x_size}*1.5"
     """Tolerance to use for the on-the-fly simplification during the prediction.
 
     Remark: you can use simple math expressions, eg. 1.5*5
@@ -552,7 +544,7 @@ class Postprocess:
     predict:simplify_algorithm.
     """
 
-    simplify_tolerance: str = '${predict:image_pixel_x_size}*2'
+    simplify_tolerance: str = "${predict:image_pixel_x_size}*2"
     """Tolerance to use for the postprocess simplification.
 
     Remark: you can use simple math expressions, eg. 1.5*5
@@ -577,7 +569,7 @@ class Dirs:
       be replaced by the default system temp dir.
     """
 
-    projects_dir: str = '..'
+    projects_dir: str = ".."
     """The base projects dir.
 
     Here, multiple orthoseg projects can be stored. Can either be:
@@ -589,55 +581,59 @@ class Dirs:
     project config file
     """
 
-    project_dir: str = '${projects_dir}/${general:segment_subject}'
+    project_dir: str = "${projects_dir}/${general:segment_subject}"
     """The project directory for this subject."""
 
-    log_dir: str = '${project_dir}/log'
+    log_dir: str = "${project_dir}/log"
     """Log dir."""
 
-    labels_dir: str = '${project_dir}/labels'
+    labels_dir: str = "${project_dir}/labels"
     """Dir containing the label data."""
 
-    training_dir: str = '${project_dir}/training'
+    training_dir: str = "${project_dir}/training"
     """Dirs used to put data during training ."""
 
-    model_dir: str = '${project_dir}/models'
+    model_dir: str = "${project_dir}/models"
     """Model dir."""
 
-    output_vector_dir: str = '${project_dir}/output_vector/${predict:image_layer}'
+    output_vector_dir: str = "${project_dir}/output_vector/${predict:image_layer}"
     """Output vector dir."""
 
-    base_image_dir: str = '${projects_dir}/_image_cache'
+    base_image_dir: str = "${projects_dir}/_image_cache"
     """Dir with the images we want predictions for."""
 
-    predict_image_input_subdir: str = '${predict:image_pixel_width}x${predict:image_pixel_height}_${predict:image_pixels_overlap}pxOverlap'  # noqa: E501
+    predict_image_input_subdir: str = "${predict:image_pixel_width}x${predict:image_pixel_height}_${predict:image_pixels_overlap}pxOverlap"  # noqa: E501
 
-    predict_image_input_dir: str = '${base_image_dir}/${predict:image_layer}/${predict_image_input_subdir}'  # noqa: E501
+    predict_image_input_dir: str = (
+        "${base_image_dir}/${predict:image_layer}/${predict_image_input_subdir}"
+    )
 
-    predict_image_output_basedir: str = '${predict_image_input_dir}'
+    predict_image_output_basedir: str = "${predict_image_input_dir}"
 
-    predictsample_image_input_subdir: str = '${train:image_pixel_width}x${train:image_pixel_height}'  # noqa: E501
+    predictsample_image_input_subdir: str = (
+        "${train:image_pixel_width}x${train:image_pixel_height}"
+    )
     """Dir with sample images for use during training.
 
     Remark: these samples are meant to check the training quality, so by default
             the train image size is used!!!
     """
 
-    predictsample_image_input_dir: str = '${base_image_dir}/${predict:image_layer}_testsample/${predictsample_image_input_subdir}'  # noqa: E501
+    predictsample_image_input_dir: str = "${base_image_dir}/${predict:image_layer}_testsample/${predictsample_image_input_subdir}"  # noqa: E501
 
-    predictsample_image_output_basedir: str = '${predictsample_image_input_dir}'
+    predictsample_image_output_basedir: str = "${predictsample_image_input_dir}"
 
 
 @dataclasses.dataclass
 class Files:
     """Settings concerning some specific file paths."""
 
-    model_json_filepath: str = '${dirs:model_dir}/${model:architecture}.json'
+    model_json_filepath: str = "${dirs:model_dir}/${model:architecture}.json"
     """File path that will be used to save/load the keras model definition."""
 
-    image_layers_config_filepath: str = '${dirs:projects_dir}/imagelayers.ini'
+    image_layers_config_filepath: str = "${dirs:projects_dir}/imagelayers.ini"
 
-    cancel_filepath: str = '${dirs:projects_dir}/cancel.txt'
+    cancel_filepath: str = "${dirs:projects_dir}/cancel.txt"
     """File path of file that if it exists cancels the current processing."""
 
 
@@ -648,13 +644,13 @@ class Email:
     enabled: bool = False
     """Set enabled to True to enable sending mails."""
 
-    from_: str = 'sample@samplemail.be'  # INI key: from
+    from_: str = "sample@samplemail.be"  # INI key: from
     """Email address to send task status info from."""
 
-    to: str = 'sample@samplemail.be'
+    to: str = "sample@samplemail.be"
     """Email address to send task status info to."""
 
-    smtp_server: str = 'server.for.emails.be'
+    smtp_server: str = "server.for.emails.be"
     """Smtp server to use ."""
 
     mail_server_username: str = ""
@@ -678,19 +674,19 @@ class Logging:
             "formatters": {
                 "console": {
                     "format": "%(asctime)s.%(msecs)03d|%(levelname)s|%(name)s|%(message)s",  # noqa: E501
-                    "datefmt": "%H:%M:%S"
+                    "datefmt": "%H:%M:%S",
                 },
                 "file": {
                     "format": "%(asctime)s|%(levelname)s|%(name)s|%(message)s",
-                    "datefmt": None
-                }
+                    "datefmt": None,
+                },
             },
             "handlers": {
                 "console": {
                     "level": "INFO",
                     "class": "logging.StreamHandler",
                     "formatter": "console",
-                    "stream": "ext://sys.stdout"
+                    "stream": "ext://sys.stdout",
                 },
                 "file": {
                     "level": "INFO",
@@ -698,32 +694,22 @@ class Logging:
                     "formatter": "file",
                     "filename": "_log/{iso_datetime}.log",
                     "maxBytes": 10000000,
-                    "backupCount": 3
-                }
+                    "backupCount": 3,
+                },
             },
             "loggers": {
                 "geofile_ops": {
                     "level": "INFO",
-                    "handlers": [
-                        "console"
-                    ],
-                    "propagate": False
+                    "handlers": ["console"],
+                    "propagate": False,
                 },
                 "geofile_ops.geofile_ops": {
                     "level": "DEBUG",
-                    "handlers": [
-                        "console"
-                    ],
-                    "propagate": False
-                }
+                    "handlers": ["console"],
+                    "propagate": False,
+                },
             },
-            "root": {
-                "level": "INFO",
-                "handlers": [
-                    "console",
-                    "file"
-                ]
-            }
+            "root": {"level": "INFO", "handlers": ["console", "file"]},
         }
     )
     """Config to use for the logging.
