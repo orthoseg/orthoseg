c7c1bc5d198535484ce792ba896c83650e9eee88
Pieter Roggemans <pieter.roggemans@gmail.com>
2026-04-16 16:21:54 +0200

    Include weight type in file name

--- helper_scripts/model_to_weights.py
+++ helper_scripts/model_to_weights.py
@@ -3,8 +3,6 @@
 import logging
 from pathlib import Path
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = "-1" # Disable using GPU
-import keras
 import segmodels_keras as smk
 
 import orthoseg.model.model_factory as mf
@@ -13,6 +11,7 @@ import orthoseg.model.model_factory as mf
 def convert_model(
     model_path: Path,
     weights_dir: Path,
+    weight_type: str,
     include_top: bool = False,
     overwrite: bool = True,
 ):
@@ -21,21 +20,22 @@ def convert_model(
     Args:
         model_path (Path): Path to the hdf5 model.
         weights_dir (Path): Directory to save the weights.
+        weight_type (str): Type of weights to save.
         include_top (bool, optional): Whether to include the top layers of the model.
             Defaults to False.
         overwrite (bool, optional): Whether to overwrite existing weights.
             Defaults to True.
-    """    
+    """
     # Load and save it in .h5 format.
     model, _ = mf.load_model(model_path, compile_model=False)
     if include_top:
-        weights_path = weights_dir / f"{model_path.stem}.weights.h5"
+        weights_path = weights_dir / f"n{model_path.stem}.weights.h5"
         model.save_weights(weights_path, overwrite=overwrite)
     else:
         # Save the weights without top layers for transfer learning.
         model_hyperparams = mf.load_model_hyperparams(model_path)
         architecture = model_hyperparams["architecture"]["architecture"]
-        weights_path = weights_dir / f"{architecture}_notop.weights.h5"
+        weights_path = weights_dir / f"{architecture}_{weight_type}_notop.weights.h5"
         _encoder, decoder = architecture.split("+")
         smk.utils.save_model_weights_notop(
             model, decoder=decoder, path=weights_path, overwrite=overwrite
@@ -45,10 +45,19 @@ def convert_model(
 # If the script is ran directly...
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    model_dir = Path(r"X:\Monitoring\OrthoSeg\sealedsurfaces\models")
+    os_dir = Path(r"X:\Monitoring\OrthoSeg")
     # mobilenetv2+linknet model
     # model_path = model_dir / "sealedsurfaces_59.5.0_0.94113_214.keras"
     # inceptionresnetv2+unet model
-    model_path = model_dir / "sealedsurfaces_60_0.97716_154.hdf5"
-    weights_dir = Path(r"X:\Monitoring\OrthoSeg\_weights")
-    convert_model(model_path, weights_dir=weights_dir, include_top=False)
+    model_path = (
+        os_dir / "recreationfields/models/recreationfields_34.2.0_0.88137_79.keras"
+    )
+
+    weights_dir = os_dir / "_weights"
+    convert_model(
+        model_path,
+        weights_dir=weights_dir,
+        weight_type="aerial",
+        include_top=False,
+        overwrite=False,
+    )
